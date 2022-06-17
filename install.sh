589d4b505b520f887fa74f6926b2369e228a6063
YOSHIDA NEKOHA <yoshida.nekoha@gmail.com>
2022-06-17 13:47:31 +0900

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 currentDir=$(cd $(dirname $0);pwd)
 
-ln -s $currentDir/home/.bash_profile ~/
-ln -s $currentDir/home/.gitconfig ~/
+if [ -e $currentDir/home/.bash_profile ];then
+    echo '.bash_profile exists.'
+else
+    ln -s $currentDir/home/.bash_profile ~/
+fi
+
+if [ "$REMOTE_CONTAINERS" == "true" ]; then
+    echo 'REMOTE_CONTAINERS, skip copy .gitconfig'
+elif [ -e $currentDir/home/.gitconfig ]; then
+    echo 'exists, skip copy .gitconfig'
+else
+    ln -s $currentDir/home/.gitconfig ~/ ;
+fi
