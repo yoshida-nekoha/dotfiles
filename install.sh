3d76b060ed6698da9e66ce1e3716dd032c99004c
YOSHIDA NEKOHA <yoshida.nekoha@gmail.com>
2022-06-19 23:21:34 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 currentDir=$(cd $(dirname $0);pwd)
 
-if [ -e $currentDir/home/.bash_profile ];then
+if [ -e ~/.bash_profile ];then
     echo '.bash_profile exists.'
 else
     ln -s $currentDir/home/.bash_profile ~/
+    source ~/.bash_profile
 fi
 
 if [ "$REMOTE_CONTAINERS" == "true" ]; then
